6d1bf8e97f27e238960fecac6a47ccf3113e9f71
Zavum <dakfunchesS@gmaill.com>
2017-01-26 20:58:00 -0500

    Writing the mediator object's methods. Started AppAdapterInterface

---
+++ app/src/main/java/xyz/encryptany/encryptany/AppAdapterInterface.java
@@ -0,0 +1,9 @@
+package xyz.encryptany.encryptany;
+
+/**
+ * Created by dakfu on 1/26/2017.
+ */
+
+public interface AppAdapterInterface {
+    public boolean sendMessage(String message);
+}
---
+++ app/src/main/java/xyz/encryptany/encryptany/Mediator.java
@@ -0,0 +1,37 @@
+package xyz.encryptany.encryptany;
+
+/**
+ * Created by dakfu on 1/26/2017.
+ */
+
+public class Mediator {
+
+
+    public Mediator(){
+
+    }
+
+    public boolean getMessages(){
+        return false;
+    }
+
+    public boolean sendMessage(){
+        return false;
+
+    }
+
+    private boolean encryptMessage(){
+        return false;
+    }
+
+    private boolean archiveMessage(){
+        return false;
+    }
+
+    private boolean displayMessage(){
+        return false;
+    }
+
+
+
+}
