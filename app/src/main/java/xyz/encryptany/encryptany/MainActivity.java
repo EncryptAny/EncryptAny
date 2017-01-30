12d855a54ae8111267520f5fd3338a5889d7e0ad
Cory <corya14@gmail.com>
2017-01-30 13:04:39 -0500

    Added ChatHead activities and listeners

--- app/src/main/java/xyz/encryptany/encryptany/MainActivity.java
+++ app/src/main/java/xyz/encryptany/encryptany/MainActivity.java
@@ -1,11 +1,18 @@
 package xyz.encryptany.encryptany;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static int OVERLAY_PERMISSION_REQ_CODE_CHATHEAD = 1234;
+    public static int OVERLAY_PERMISSION_REQ_CODE_CHATHEAD_MSG = 5678;
     public static Button btnStartService, btnShowMsg;
 
     @Override
@@ -15,5 +22,57 @@ public class MainActivity extends AppCompatActivity {
 
         btnStartService = (Button)findViewById(R.id.btnStartService);
         btnShowMsg = (Button)findViewById(R.id.btnMsg);
+
+        btnStartService.setOnClickListener(lst_StartService);
+        btnShowMsg.setOnClickListener(lst_ShowMsg);
+    }
+
+    Button.OnClickListener lst_StartService = new Button.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            // TODO Auto-generated method stub
+            Log.d(Utils.LogTag, "lst_StartService -> Utils.canDrawOverlays(Main.this): " + Utils.canDrawOverlays(MainActivity.this));
+
+            if(Utils.canDrawOverlays(MainActivity.this))
+                startChatHead();
+            else{
+                requestPermission(OVERLAY_PERMISSION_REQ_CODE_CHATHEAD);
+            }
+        }
+
+    };
+
+    Button.OnClickListener lst_ShowMsg = new Button.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            // TODO Auto-generated method stub
+            if(Utils.canDrawOverlays(MainActivity.this))
+                showChatHeadMsg();
+            else{
+                requestPermission(OVERLAY_PERMISSION_REQ_CODE_CHATHEAD_MSG);
+            }
+
+        }
+
+    };
+
+    private void startChatHead(){
+        startService(new Intent(MainActivity.this, ChatHeadService.class));
+    }
+    private void showChatHeadMsg(){
+        java.util.Date now = new java.util.Date();
+        String str = "test by henry  " + new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(now);
+
+        Intent it = new Intent(MainActivity.this, ChatHeadService.class);
+        it.putExtra(Utils.EXTRA_MSG, str);
+        startService(it);
+    }
+
+    private void requestPermission(int requestCode){
+        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+        intent.setData(Uri.parse("package:" + getPackageName()));
+        startActivityForResult(intent, requestCode);
     }
 }
