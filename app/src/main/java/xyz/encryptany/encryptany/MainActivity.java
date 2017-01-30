ad60c18145c5af4078085c402a926bd7ef4c5138
Cory <corya14@gmail.com>
2017-01-30 12:13:01 -0500

    Added buttons to MainActivity

--- app/src/main/java/xyz/encryptany/encryptany/MainActivity.java
+++ app/src/main/java/xyz/encryptany/encryptany/MainActivity.java
@@ -2,13 +2,18 @@ package xyz.encryptany.encryptany;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static Button btnStartService, btnShowMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnStartService = (Button)findViewById(R.id.btnStartService);
+        btnShowMsg = (Button)findViewById(R.id.btnMsg);
     }
 }
