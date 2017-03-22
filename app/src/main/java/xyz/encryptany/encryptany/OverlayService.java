8a72514681eba97b14cbc6dfb3a0f9a7bfb5d2f9
Cory <corya14@gmail.com>
2017-03-22 12:33:10 -0400

    Added working overlay recyclerView

--- app/src/main/java/xyz/encryptany/encryptany/OverlayService.java
+++ app/src/main/java/xyz/encryptany/encryptany/OverlayService.java
@@ -119,7 +119,7 @@ public class OverlayService extends Service {
 
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
+                300,
                 WindowManager.LayoutParams.TYPE_PHONE,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 PixelFormat.TRANSLUCENT);
