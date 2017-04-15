b5047da9abc948af5ca8bd37fa5f37f2df201300
Dakota Funchess <dakfunchess@gmail.com>
2017-04-15 19:10:56 -0400

    fixing minor bugs clean build missed

--- app/src/main/java/xyz/encryptany/encryptany/Overlay.java
+++ app/src/main/java/xyz/encryptany/encryptany/Overlay.java
@@ -24,7 +24,7 @@ public class Overlay extends Activity {
         EncryptedMessage[] dummydata = new EncryptedMessage[dummyAmount];
         for (int i = 1; i != dummyAmount+1; ++i)
         {
-            dummydata[i-1] = new EncryptedMessage("Dummy encrypted text " + i,"Dummy source " + i,"Dummy app " + i, new Date().getTime(), UUID.randomUUID().toString());
+            dummydata[i-1] = new EncryptedMessage("Dummy encrypted text " + i,"Dummy source " + i,"Dummy app " + i, new Date().getTime(), UUID.randomUUID().toString(),null);
         }
         return dummydata;
     }
